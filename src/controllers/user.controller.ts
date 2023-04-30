4a35716e4e7ac3f693ad80a7f757a6b329b6e695
Tulio <tuliohcosta@gmail.com>
2023-04-30 00:00:17 -0300

    return auth token after signin sucessfully

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,15 +1,23 @@
+import jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
 import { UserModel } from '../models/user.model';
 
 export default {
   async signin(req: Request, res: Response) {
+    const secret = process.env.JWT_SECRET || 'secret';
+    
     try {
       const { username, password } = req.body;
       const user = await UserModel.findOne({ username, password });
       if (!user) {
         return res.status(401).json({ error: 'Invalid credentials' });
       }
-      res.json({ message: 'Authentication successful' });
+  
+      // Create a JWT token with the user ID and username
+      const token = jwt.sign({ userId: user._id, username: user.username }, secret);
+  
+      // Return the token as part of the response
+      res.json({ token });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'An unexpected error occurred' });
