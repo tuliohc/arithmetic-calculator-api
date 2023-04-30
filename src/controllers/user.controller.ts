32791732791c6ddd21ffe6559ca434ec299a4d76
Tulio <tuliohcosta@gmail.com>
2023-04-29 21:56:24 -0300

    deal with unexpected error 500 on signin call

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,11 +3,16 @@ import { UserModel } from '../models/user.model';
 
 export default {
   async signin(req: Request, res: Response) {
-    const { username, password } = req.body;
-    const user = await UserModel.findOne({ username, password });
-    if (!user) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+    try {
+      const { username, password } = req.body;
+      const user = await UserModel.findOne({ username, password });
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+      res.json({ message: 'Authentication successful' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An unexpected error occurred' });
     }
-    res.json({ message: 'Authentication successful' });
-  },
+  }
 };
