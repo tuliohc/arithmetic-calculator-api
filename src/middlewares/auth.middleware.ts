ff4f3d167b8c987dd02d7ce70e3a5f2ad9d070fd
Tulio <tuliohcosta@gmail.com>
2023-04-29 23:59:02 -0300

    add requireAuth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+
+interface AuthRequest extends Request {
+  userId?: string;
+}
+
+export function requireAuth(req: AuthRequest, res: Response, next: NextFunction) {
+  // Get the token from the Authorization header
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Authentication required' });
+  }
+  const token = authHeader.split(' ')[1];
+
+  // Verify the token and extract the user ID
+  try {
+    const decoded = jwt.verify(token, 'secret');
+    req.userId = (decoded as any).userId;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ error: 'Invalid token' });
+  }
+}
