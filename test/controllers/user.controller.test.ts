293afa38cbd9bea23420a38ec50204e9da274548
Tulio <tuliohcosta@gmail.com>
2023-05-01 13:00:46 -0300

    add users balance tests

--- test/controllers/user.controller.test.ts
+++ test/controllers/user.controller.test.ts
@@ -2,18 +2,24 @@ import { Request, Response } from 'express';
 import userController from '../../src/controllers/user.controller';
 import { UserModel } from '../../src/models/user.model';
 
-function createRequest(username: string, password: string): Request {
+interface AuthenticatedRequest extends Request {
+  userId?: string;
+}
+
+function createAuthRequest(username: string, password: string): Request {
   return {
     body: { username, password },
   } as Request;
 }
 
 function createResponse(): Partial<Response> {
-  return {
+  const res = {
     status: jest.fn().mockReturnThis(),
     json: jest.fn(),
     clearCookie: jest.fn(),
   };
+  res.status.mockReturnValue(res);
+  return res;
 }
 
 describe('User controller', () => {
@@ -31,7 +37,7 @@ describe('User controller', () => {
     });
 
     it('should respond with a 200 status code for valid credentials', async () => {
-      const req = createRequest('user@example.com', 'password123');
+      const req = createAuthRequest('user@example.com', 'password123');
       const res = createResponse() as unknown as Response;
       
       findOneStub.mockResolvedValueOnce({
@@ -46,7 +52,7 @@ describe('User controller', () => {
     });
 
     it('should respond with a 401 status code for invalid credentials', async () => {
-      const req = createRequest('user@example.com','wrongpassword')
+      const req = createAuthRequest('user@example.com','wrongpassword')
       const res = createResponse() as unknown as Response;
 
       findOneStub.mockResolvedValueOnce(null);
@@ -58,7 +64,7 @@ describe('User controller', () => {
     });
 
     it('should respond with a 500 status code for unexpected errors', async () => {
-      const req = createRequest('user@example.com', 'password123');
+      const req = createAuthRequest('user@example.com', 'password123');
       const res = createResponse() as unknown as Response;
     
       findOneStub.mockRejectedValueOnce(new Error('An unexpected error occurred'));
@@ -80,6 +86,54 @@ describe('User controller', () => {
 
       expect(res.clearCookie).toHaveBeenCalledWith('token');
       expect(res.json).toHaveBeenCalledWith({ message: 'Logout successful' });
+    });
+  });
+
+  describe('getUserBalance', () => {
+    const mockUser = {
+      id: 'mockUserId',
+      balance: '100',
+    };
+    const req = {
+      userId: 'mockUserId',
+      params: { id: 'mockUserId' }
+    } as unknown as AuthenticatedRequest;
+
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it('should return the user balance', async () => {
+      const res = createResponse() as unknown as Response;
+      jest.spyOn(UserModel, 'findById').mockResolvedValueOnce(mockUser);
+
+      await userController.getUserBalance(req, res);
+
+      expect(UserModel.findById).toHaveBeenCalledWith(mockUser.id);
+      expect(res.json).toHaveBeenCalledWith({ balance: mockUser.balance });
+    });
+
+    it('should return 404 when user is not found', async () => {
+      const res = createResponse() as unknown as Response;
+      const findByIdSpy = jest.spyOn(UserModel, 'findById').mockResolvedValueOnce(null);
+    
+      await userController.getUserBalance(req, res);
+    
+      expect(findByIdSpy).toHaveBeenCalledWith(req.userId);
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.json).toHaveBeenCalledWith({ error: 'User not found' });
+    });
+
+    it('should return 500 when an unexpected error occurs', async () => {
+      const res = createResponse() as unknown as Response;
+      jest.spyOn(UserModel, 'findById').mockRejectedValueOnce(new Error('An unexpected error occurred'));
+
+      await userController.getUserBalance(req, res);
+
+      expect(UserModel.findById).toHaveBeenCalledWith(req.userId);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({ error: 'An unexpected error occurred' });
     });
   });
 });
