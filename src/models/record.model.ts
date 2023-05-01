303c97b9d1d428029b774ed6916ebf391ce0d9b0
Tulio <tuliohcosta@gmail.com>
2023-04-30 22:32:10 -0300

    add record model

---
+++ src/models/record.model.ts
@@ -0,0 +1,55 @@
+import { Document, model, Schema } from 'mongoose';
+import { Operation } from './operation.model';
+import { User } from './user.model';
+
+interface Record extends Document {
+  operation: Operation['_id'];
+  user: User['_id'];
+  amount: string;
+  userBalance: string;
+  operationResponse: string;
+  date: Date;
+  deletedAt?: Date;
+}
+
+const RecordSchema = new Schema({
+  operation: {
+    type: Schema.Types.ObjectId,
+    ref: 'Operation',
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  amount: {
+    type: String,
+    required: true,
+  },
+  userBalance: {
+    type: String,
+    required: true,
+  },
+  operationResponse: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  deletedAt: {
+    type: Date,
+    default: null,
+  },
+});
+
+RecordSchema.virtual('isDeleted').get(function () {
+  return !!this.deletedAt;
+});
+
+const RecordModel = model<Record>('Record', RecordSchema);
+
+export { Record, RecordModel };
