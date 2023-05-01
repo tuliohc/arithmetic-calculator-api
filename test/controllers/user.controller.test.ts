1f6579141456309b4348fbb270e8958e5e15bd24
Tulio <tuliohcosta@gmail.com>
2023-04-30 22:43:09 -0300

    refactor: code duplicity removed

--- test/controllers/user.controller.test.ts
+++ test/controllers/user.controller.test.ts
@@ -8,14 +8,32 @@ function createRequest(username: string, password: string): Request {
   } as Request;
 }
 
+function createResponse(): Partial<Response> {
+  return {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+    clearCookie: jest.fn(),
+  };
+}
+
 describe('User controller', () => {
   describe('signin', () => {
+    let findOneStub: jest.SpyInstance;
+
+    beforeEach(() => {
+      // create a stub for UserModel.findOne method
+      findOneStub = jest.spyOn(UserModel, 'findOne');
+    });
+
+    afterEach(() => {
+      // restore original method
+      findOneStub.mockRestore();
+    });
+
     it('should respond with a 200 status code for valid credentials', async () => {
       const req = createRequest('user@example.com', 'password123');
-      const res = { json: jest.fn() } as unknown as Response;
+      const res = createResponse() as unknown as Response;
       
-      // create a stub for UserModel.findOne method
-      const findOneStub = jest.spyOn(UserModel, 'findOne');
       findOneStub.mockResolvedValueOnce({
         id: '123',
         username: 'user@example.com',
@@ -25,51 +43,30 @@ describe('User controller', () => {
       await userController.signin(req, res);
 
       expect(res.json).toHaveBeenCalledWith({ token: expect.any(String) });
-      
-      // restore original method
-      findOneStub.mockRestore();
     });
 
     it('should respond with a 401 status code for invalid credentials', async () => {
-      const req = {
-        body: { username: 'user@example.com', password: 'wrongpassword' },
-      } as Request;
-      const res = { 
-        status: jest.fn().mockReturnThis(), 
-        json: jest.fn() 
-      } as unknown as Response;
+      const req = createRequest('user@example.com','wrongpassword')
+      const res = createResponse() as unknown as Response;
 
-      // create a stub for UserModel.findOne method
-      const findOneStub = jest.spyOn(UserModel, 'findOne');
       findOneStub.mockResolvedValueOnce(null);
 
       await userController.signin(req, res);
 
       expect(res.status).toHaveBeenCalledWith(401);
       expect(res.json).toHaveBeenCalledWith({ error: 'Invalid credentials' });
-
-      // restore original method
-      findOneStub.mockRestore();
     });
 
     it('should respond with a 500 status code for unexpected errors', async () => {
       const req = createRequest('user@example.com', 'password123');
-      const res = { 
-        status: jest.fn().mockReturnThis(), 
-        json: jest.fn() 
-      } as unknown as Response;
+      const res = createResponse() as unknown as Response;
     
-      // // create a stub for UserModel.findOne method that throws an error
-      const findOneStub = jest.spyOn(UserModel, 'findOne');
       findOneStub.mockRejectedValueOnce(new Error('An unexpected error occurred'));
     
       await userController.signin(req, res);
     
       expect(res.status).toHaveBeenCalledWith(500);
       expect(res.json).toHaveBeenCalledWith({ error: 'An unexpected error occurred' });
-    
-      // restore original method
-      findOneStub.mockRestore();
     });
 
   });
@@ -77,10 +74,7 @@ describe('User controller', () => {
   describe('logout', () => {
     it('should clear the token cookie and return a success message', () => {
       const req = { headers: { authorization: 'Bearer valid-token' } } as Request;
-      const res = {
-        clearCookie: jest.fn(),
-        json: jest.fn(),
-      } as unknown as Response;
+      const res = createResponse() as unknown as Response;
 
       userController.logout(req, res);
 
