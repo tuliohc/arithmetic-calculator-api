1fe5513e890b18a31fb19bc8da173e5f58bfc520
Tulio <tuliohcosta@gmail.com>
2023-05-01 13:00:16 -0300

    refactor auth middleware test

--- test/middlewares/auth.middleware.test.ts
+++ test/middlewares/auth.middleware.test.ts
@@ -13,7 +13,10 @@ describe('requireAuth Middleware', () => {
 
   it('should set the userId on the request object when given a valid token', () => {
     const token = jwt.sign({ userId: '123' }, secret);
-    const req = { headers: { authorization: `Bearer ${token}` } } as unknown as AuthRequest;
+    const req = { 
+      headers: { authorization: `Bearer ${token}` },
+      params: { id: '123' } 
+    } as unknown as AuthRequest;
     const res = {} as Response;
     const next = jest.fn() as NextFunction;
 
